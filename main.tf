a18e7425fafd6b6419213e8ab67225e0c4343bea
jghlee <89275751+jghlee@users.noreply.github.com>
2024-05-15 21:28:20 -0400

    change http listener

--- main.tf
+++ main.tf
@@ -54,28 +54,6 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-    
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
   target_groups = {
     ex-instance = {
       name_prefix      = "blog-"
@@ -91,10 +69,19 @@ module "alb" {
   }
 
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Environment = "dev"
   }
 }
+
+http_tcp_listeners = [
+  {
+    port = 80
+    protocol = "HTTP"
+    target_group_index = 0
+  }
+]
+
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
